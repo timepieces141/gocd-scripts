abfbc8b0a6e4f838da95019d7bf2c7943dec7d51
Edward Petersen <edward.petersen@gmail.com>
2014-08-23 17:37:34 -0400

    new script to generate a dockerfile based on a given template and key=value pairs that will be replaced in the template; generate_dockerfile.sh always does a sed replace for the VERSION of the software, as dictated by the constants file and GO_PIPELINE_COUNT (docker image and software have same version number)

---
+++ generate_dockerfile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# exit on any error
+set -e
+
+# first argument is the path to the dockerfile.template
+TEMPLATE=$1
+if [ ! -f "$TEMPLATE" ]; then
+	echo "Template file given either doesn't exist or is not a file"
+	exit 1
+fi
+shift
+
+# VERSION is always handled by this script, the value sourced by constants.sh
+.  constants.sh
+
+# copy template locally as "Dockerfile"
+cp $TEMPLATE Dockerfile
+
+# perform version sed
+echo "Changing the Version of the Dockerfile to $VERSION"
+sed -i "s/<<VERSION>>/$VERSION/g" Dockerfile
+
+# loop through the rest of the replacement pairs and perform the sed operation
+while [ "$1" != "" ]; do
+	# double escape forward slashes
+	ESCAPED="${1//\//\\/}"
+	echo "Changing <<${ESCAPED%=*}>> to ${ESCAPED#*=}"
+	sed -i "s/<<${ESCAPED%=*}>>/${ESCAPED#*=}/g" Dockerfile
+	shift
+done
